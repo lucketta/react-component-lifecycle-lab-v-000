14eae0aa9a59ee18341df937033a20114c7ab9c4
lucketta <lucketta@umich.edu>
2017-08-24 17:12:55 -0400

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -4,16 +4,26 @@ import Tweet from './Tweet';
 class TweetWall extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       tweets: []
     };
   }
 
   // TODO: componentWillMount()
+  componentWillMount = () => {
+    this.state.tweets = this.props.newTweets;
+  }
   // TODO: shouldComponentUpdate()
+  shouldComponentUpdate = (nextProps) => {
+    return nextProps.newTweets.length > 0;
+  }
   // TODO: componentWillReceiveProps()
-
+  componentWillReceiveProps = (nextProps) => {
+    this.setState({
+      tweets: [...nextProps.newTweets, ...this.state.tweets],
+    });
+  }
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
 
